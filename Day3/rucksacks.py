7453a8aa0402684856036e12d7807698f22eb980
Mark Snyder <mark.clifford.snyder@gmail.com>
2022-12-03 00:20:05 -0500

    day3 star2

--- Day3/rucksacks.py
+++ Day3/rucksacks.py
@@ -12,3 +12,20 @@ with open('input.txt','r') as input:
             total_priority += ord(intersection[0]) - ord('A') + 27
 
 print(total_priority)
+
+# Star 2
+with open('input.txt','r') as input:
+    total_priority = 0
+    group_rucksacks = []
+    for rucksack in input:
+        contents = rucksack.strip()
+        group_rucksacks.append(set(contents))
+        if len(group_rucksacks) == 3:
+            intersection = list(group_rucksacks[0].intersection(group_rucksacks[1]).intersection(group_rucksacks[2]))
+            if ord(intersection[0]) >= 97:
+                total_priority += ord(intersection[0]) - ord('a') + 1
+            else:
+                total_priority += ord(intersection[0]) - ord('A') + 27
+            group_rucksacks = []
+
+print(total_priority)
