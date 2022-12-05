d660123fc1bfcca57c0602747af89b5db397de40
Mark Snyder <mark.clifford.snyder@gmail.com>
2022-12-05 00:29:44 -0500

    day5 star2

--- Day5/supply_stacks.py
+++ Day5/supply_stacks.py
@@ -1,5 +1,10 @@
 from collections import deque
 
+with open('input.txt','r') as input:
+    input_lines = input.readlines()
+input_lines = input_lines[10:]
+
+# Star 1
 stacks = [[None],
           deque(['G','T','R','W']),
           deque(['G','C','H','P','M','S','V','W']),
@@ -10,16 +15,31 @@ stacks = [[None],
           deque(['Z','B','D','F','G','C','S','J']),
           deque(['R','T','B']),
           deque(['H','N','W','L','C'])]
-
-with open('input.txt','r') as input:
-    input_lines = input.readlines()
-input_lines = input_lines[10:]
-
-# Star 1
+          
 for line in input_lines:
     line = line.split()
     num_crates,source,dest = int(line[1]),int(line[3]),int(line[5])
     for _ in range(num_crates):
         stacks[dest].append(stacks[source].pop())
+
+print(''.join([stack[-1] for stack in stacks[1:]]))
+
+# Star 2
+stacks = [[None],
+          ['G','T','R','W'],
+          ['G','C','H','P','M','S','V','W'],
+          ['C','L','T','S','G','M'],
+          ['J','H','D','M','W','R','F'],
+          ['P','Q','L','H','S','W','F','J'],
+          ['P','J','D','N','F','M','S'],
+          ['Z','B','D','F','G','C','S','J'],
+          ['R','T','B'],
+          ['H','N','W','L','C']]
+
+for line in input_lines:
+    line = line.split()
+    num_crates,source,dest = int(line[1]),int(line[3]),int(line[5])
+    stacks[dest].extend(stacks[source][-num_crates:])
+    stacks[source] = stacks[source][:-num_crates]
 
 print(''.join([stack[-1] for stack in stacks[1:]]))
