3dc06b9d57e958039f2939a2a2127229b3aae8a7
Mark Snyder <mark.clifford.snyder@gmail.com>
2022-12-09 01:40:47 -0500

    day9 star2

--- Day9/ropebridge.py
+++ Day9/ropebridge.py
@@ -1,42 +1,56 @@
-def move_up(head,tail):
-    head[1] += 1
-    if head[1]-tail[1] > 1:
-        tail[0] = head[0]
-        tail[1] = head[1]-1
+from math import copysign
 
-def move_down(head,tail):
-    head[1] -= 1
-    if tail[1]-head[1] > 1:
-        tail[0] = head[0]
-        tail[1] = head[1]+1
+def chase(target,segment):
+    hori_dist = target[0] - segment[0]
+    vert_dist = target[1] - segment[1]
+    if abs(hori_dist) > 1:
+        segment[0] += copysign(1,hori_dist)
+        if vert_dist:
+            segment[1] += copysign(1,vert_dist)
+    elif abs(vert_dist) > 1:
+        segment[1] += copysign(1,vert_dist)
+        if hori_dist:
+            segment[0] += copysign(1,hori_dist)
 
-def move_left(head,tail):
-    head[0] -= 1
-    if tail[0]-head[0] > 1:
-        tail[1] = head[1]
-        tail[0] = head[0]+1
-
-def move_right(head,tail):
-    head[0] += 1
-    if head[0]-tail[0] > 1:
-        tail[1] = head[1]
-        tail[0] = head[0]-1
+def move_rope(head,tail,direction):
+    match direction:
+        case 'U':
+            head[1] += 1
+        case 'D':
+            head[1] -= 1
+        case 'R':
+            head[0] += 1
+        case 'L':
+            head[0] -= 1
+    for idx,segment in enumerate(tail):
+        if idx == 0:
+            target = head
+        else:
+            target = tail[idx-1]
+        chase(target,segment)
 
 with open('input.txt','r') as input:
     input_lines = input.read().splitlines()
 
-directions = ['U','D','L','R']
-move_fns = [move_up,move_down,move_left,move_right]
-
 tail_locations = set([(0,0)])
 head = [0,0]
-tail = [0,0]
+tail = [[0,0]]
+
+longtail_locations = set([(0,0)])
+longhead = [0,0]
+longtail = [[0,0] for _ in range(9)]
 for line in input_lines:
     direction,magnitude = line.split()
     magnitude = int(magnitude)
-    move_fn = move_fns[directions.index(direction)]
     for _ in range(magnitude):
-        move_fn(head,tail)
-        tail_locations.add(tuple(tail))
+        move_rope(head,tail,direction)
+        tail_locations.add(tuple(tail[-1]))
 
+        move_rope(longhead,longtail,direction)
+        longtail_locations.add(tuple(longtail[-1]))
+
+# Star 1
 print(len(tail_locations))
+
+# Star 2
+print(len(longtail_locations))
