31244c1c1e3b8b2704b35cbd1f01fc2c3e072262
Mark Snyder <mark.clifford.snyder@gmail.com>
2022-12-17 06:40:51 -0500

    day17 star2 working for test but not full input

--- Day17/tetris.py
+++ Day17/tetris.py
@@ -9,15 +9,16 @@ def piece_generator():
     pieces = [piece1,piece2,piece3,piece4,piece5]
     count = 0
     while True:
-        yield pieces[count % 5]
-        count += 1
+        yield pieces[count],count
+        count = (count+1) % 5
 
 def jet_generator(jets):
     num_jets = len(jets)
     count = 0
     while True:
-        yield 1 if jets[count % num_jets] == '>' else -1
-        count += 1
+        value = 1 if jets[count] == '>' else -1
+        yield value,count
+        count = (count+1) % num_jets
 
 def valid(chamber,candidate_places):
     try:
@@ -30,15 +31,20 @@ def valid(chamber,candidate_places):
         return False
     return True
 
-def drop_piece(chamber,current_depth,piece_gen,jet_gen):
+def drop_piece(chamber,current_depth,piece_gen,jet_gen,cache,piece_no,v=False):
+
+    if not piece_no:
+        initial_state = tuple([0,0,0,0,0,0,0])
+    else:
+        initial_state = tuple(chamber[current_depth])
 
     start_point = np.array((current_depth-4,2))
-    piece = next(piece_gen)
+    piece,_ = next(piece_gen)
 
     current_spaces = [tuple(start_point+offset) for offset in piece]
     new_depth = current_depth-4+min([offset[0] for offset in piece])
     while True:
-        jet = next(jet_gen)
+        jet,last_jet_idx = next(jet_gen)
         next_spaces = [tuple([space[0],space[1]+jet]) for space in current_spaces]
         if valid(chamber,next_spaces):
             current_spaces = next_spaces
@@ -51,9 +57,15 @@ def drop_piece(chamber,current_depth,piece_gen,jet_gen):
     for space in current_spaces:
         chamber[space] = 1
 
-    return new_depth
+    final_state = tuple(chamber[new_depth])
+    states = tuple([initial_state,final_state])
+    if v and states in cache and initial_state != final_state:
+        print('possible cycle of length', piece_no-cache[states],'pieces')
+    cache[states] = piece_no
 
-with open('input.txt','r') as input_file:
+    return new_depth, last_jet_idx
+
+with open('input_test1.txt','r') as input_file:
     jets = input_file.readlines()[0][:-1]
 
 piece_gen = piece_generator()
@@ -61,6 +73,46 @@ jet_gen = jet_generator(jets)
 
 chamber = np.zeros((10000,7))
 current_depth = 10000
+cache = {}
 for piece_no in range(2022):
-    current_depth = drop_piece(chamber,current_depth,piece_gen,jet_gen)
+    current_depth,_ = drop_piece(chamber,current_depth,piece_gen,jet_gen,cache,piece_no)
 print(10000-current_depth)
+
+# Star 2
+piece_gen = piece_generator()
+jet_gen = jet_generator(jets)
+chamber = np.zeros((10000,7))
+current_depth = 10000
+cache = {}
+for piece_no in range(300):
+    current_depth,_ = drop_piece(chamber,current_depth,piece_gen,jet_gen,cache,piece_no,True)
+cycle_block_length = 35 # by inspection
+
+test_length = 8
+cycle_start_depth = 0
+cycle_lengths = set()
+for row_idx in range(current_depth,chamber.shape[0]-test_length):
+    test_array = chamber[row_idx:row_idx+test_length]
+    for offset in range(test_length,chamber.shape[0]-test_length-row_idx):
+        if (test_array == chamber[row_idx+offset:row_idx+offset+test_length]).all():
+            print('likely cycle of height',offset)
+            cycle_lengths.add(offset)
+            cycle_start_depth = row_idx+test_length+offset-1
+cycle_height = 53 # by inspection
+print(cycle_start_depth)
+
+elephant_requested_blocks = 1000000000000
+num_cycles = elephant_requested_blocks // cycle_block_length - 10 # fudge factor since idk how many blocks the startup takes
+
+blocks_remaining = elephant_requested_blocks - num_cycles*cycle_block_length
+
+piece_gen = piece_generator()
+jet_gen = jet_generator(jets)
+chamber = np.zeros((10000,7))
+current_depth = 10000
+cache = {}
+for piece_no in range(blocks_remaining):
+    current_depth,_ = drop_piece(chamber,current_depth,piece_gen,jet_gen,cache,piece_no)
+extra_height = 10000-current_depth
+
+print(num_cycles*cycle_height+extra_height)
