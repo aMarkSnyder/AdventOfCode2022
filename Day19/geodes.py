9155a0f5c9b6a9b81fd0b9d9a9f716b59d498629
Mark Snyder <mark.clifford.snyder@gmail.com>
2022-12-19 05:37:49 -0500

    day19 star2

--- Day19/geodes.py
+++ Day19/geodes.py
@@ -1,4 +1,5 @@
 import re
+from math import prod
 from tqdm import tqdm
 
 class Sim():
@@ -55,16 +56,14 @@ class Sim():
         return True
 
     def build(self,robot_type):
-        if robot_type == 'end':
-            return self.fast_forward()
         if not self.can_ever_build(robot_type):
             return -1
         start_time = self.time
         while not self.can_build(robot_type) and self.time < self.max_time:
             self.produce()
             self.time += 1
-        if self.time > self.max_time:
-            return -1
+        if self.time == self.max_time:
+            return self.time - start_time
         self.produce()
         self.time += 1
         if robot_type:
@@ -73,68 +72,48 @@ class Sim():
             self.production[robot_type] += 1
         return self.time - start_time
 
-    def time_to_build(self,robot_type):
-        state = self.get_state()
-        time = self.build(robot_type)
-        self.load_state(state)
-        return time
-
-    def fast_forward(self):
-        start_time = self.time
-        self.time = self.max_time
-        time_diff = self.time - start_time
-        for mat,gain in self.production.items():
-            self.stored[mat] += time_diff*gain
-        return time_diff
-
-
-# def generate_valid_routes(all_paths,curr_path,curr_valve,valuable_valves,visited_valves,distances,time,max_time):
-#     if time >= max_time:
-#         all_paths.add(tuple(curr_path))
-#         return
-#     if set([curr_valve]) == valuable_valves-visited_valves:
-#         all_paths.add(tuple(curr_path+[curr_valve]))
-#         return
-#     if curr_valve in visited_valves:
-#         return
-#     curr_path = curr_path + [curr_valve]
-#     visited_valves = visited_valves.union({curr_valve})
-#     for valve in valuable_valves-visited_valves:
-#         generate_valid_routes(all_paths,curr_path,valve,valuable_valves,visited_valves,distances,time+distances[curr_valve][valve],max_time)
-
-def generate_valid_routes(all_paths,curr_path,curr_action,blueprint,max_time,state):
+def count_geodes(geode_counts,next_bot,blueprint,max_time,state):
     if state[-1] == max_time:
-        all_paths.add(state[0]['geode'])
+        geode_counts.add(state[0]['geode'])
         return
     sim = Sim(blueprint,max_time,state)
-    if sim.build(curr_action) == -1:
-        #print(curr_action,'will take too long after',curr_path)
+    if sim.build(next_bot) == -1:
         return
-    curr_path = curr_path + [curr_action]
-    valuable_actions = []
-    if sim.production['ore'] < 6:
-        valuable_actions.append('ore')
+    valuable_bots = []
+    if sim.production['ore'] < 4:
+        valuable_bots.append('ore')
     if sim.production['clay'] < 10:
-        valuable_actions.append('clay')
+        valuable_bots.append('clay')
     if sim.can_ever_build('obs'):
-        valuable_actions.append('obs')
+        valuable_bots.append('obs')
     if sim.can_ever_build('geode'):
-        valuable_actions.append('geode')
-    if sim.production['geode'] > 0:
-        valuable_actions.append('end')
+        valuable_bots.append('geode')
+    if sim.can_build('obs'):
+        valuable_bots = ['clay','obs']
     if sim.can_build('geode'):
-        valuable_actions = ['geode']
-    for action in valuable_actions:
-        generate_valid_routes(all_paths,curr_path,action,blueprint,max_time,sim.get_state())
+        valuable_bots = ['geode']
+    for bot in valuable_bots:
+        count_geodes(geode_counts,bot,blueprint,max_time,sim.get_state())
 
 with open('input.txt','r') as input_file:
     blueprints = input_file.readlines()
 
+# Star 1
 max_geodes = []
 for blueprint in tqdm(blueprints):
     base_sim = Sim(blueprint,24)
     geodes = set()
-    generate_valid_routes(geodes,[],None,blueprint,24,base_sim.get_state())
+    count_geodes(geodes,None,blueprint,24,base_sim.get_state())
     max_geodes.append(max(geodes))
 print(max_geodes)
 print(sum([idx*count for idx,count in enumerate(max_geodes,start=1)]))
+
+# Star 2
+max_geodes = []
+for blueprint in tqdm(blueprints[:3]):
+    base_sim = Sim(blueprint,32)
+    geodes = set()
+    count_geodes(geodes,None,blueprint,32,base_sim.get_state())
+    max_geodes.append(max(geodes))
+print(max_geodes)
+print(prod(max_geodes))
